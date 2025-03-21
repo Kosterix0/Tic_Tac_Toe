8be1e2b328cbf608226c181b885abb6f26c23b2a
Kosterix0 <ksawerymiszczakgit@gmail.com>
2025-03-21 21:27:06 +0100

    board, playing, gameover screen are done

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,27 +1,31 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
-  let editablePlayerName = <span className="player-name">{playerName}</span>;
-
   function handleEditClick() {
     setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(event) {
     setPlayerName(event.target.value);
   }
 
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
   if (isEditing) {
     editablePlayerName = (
-      <input
-        type="text"
-        required
-        defaultValue={playerName}
-        onChange={handleChange}
-      />
+      <input type="text" required value={playerName} onChange={handleChange} />
     );
   }
 
